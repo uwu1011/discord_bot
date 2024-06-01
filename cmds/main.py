6f925f574b5d4a00c6be3b2d27be8010f4ac742e
Alan Chang <menko9487@gmail.com>
2024-06-01 18:50:08 +0800

    list todo

--- cmds/main.py
+++ cmds/main.py
@@ -4,16 +4,28 @@ import json
 from core import Cog_Extension
 
 class Main(Cog_Extension):
-        
+
+    todos = {}
     @commands.command()
     async def Hello(self, ctx):
         await ctx.send("Hello, world")
     
     @commands.command()
     async def AddTodoList(self, ctx, item):
-        todo = []
-        todo.append(item)
-        await ctx.send("Successfully Added!")
+        user_id = str(ctx.author.id)
+        if user_id not in todos:
+            todos[user_id] = []
+        todos[user_id].append(item)
+        await ctx.send(f'Task added: {item}')
+
+    @commands.command()
+    async def ListTodoList(ctx):
+        user_id = str(ctx.author.id)
+        if user_id not in todos or not todos[user_id]:
+            await ctx.send('You have no tasks in your to-do list.')
+        else:
+            tasks = '\n'.join(f'{idx + 1}. {task}' for idx, task in enumerate(todos[user_id]))
+            await ctx.send(f'Your to-do list:\n{tasks}')
     '''
     TODO
     Add the necessary bot commands here
