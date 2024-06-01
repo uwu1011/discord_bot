a099d73c822a97bdd0c8c9e1b25fb4a04c1ad869
Alan Chang <menko9487@gmail.com>
2024-06-01 20:52:33 +0800

    global

--- cmds/main.py
+++ cmds/main.py
@@ -12,15 +12,17 @@ class Main(Cog_Extension):
     
     
     @commands.command()
-    async def AddTodoList(self, ctx, item, todos):
+    async def AddTodoList(self, ctx, item):
         user_id = str(ctx.author.id)
+        global todos
         if user_id not in todos:
             todos[user_id] = []
         todos[user_id].append(item)
 
     @commands.command()
-    async def ListTodoList(self, ctx, todos):
+    async def ListTodoList(self, ctx):
         user_id = str(ctx.author.id)
+        global todos
         if user_id not in todos or not todos[user_id]:
             await ctx.send('You have no tasks in your to-do list.')
         else:
