02c27b50c77bd9ce5c75dfed82bf4b9ba5a1a5a5
Alan Chang <menko9487@gmail.com>
2024-06-01 21:21:17 +0800

    todo list done ez shit

--- cmds/main.py
+++ cmds/main.py
@@ -21,13 +21,29 @@ class Main(Cog_Extension):
         await ctx.send(f'Successfully added: {item}')
 
     @commands.command()
-    async def ListTodoList(self, ctx):
+    async def ShowTodoList(self, ctx):
         user_id = str(ctx.author.id)
         if user_id not in todos or not todos[user_id]:
             await ctx.send('You have no tasks in your to-do list.')
         else:
             tasks = '\n'.join(f'{idx + 1}. {task}' for idx, task in enumerate(todos[user_id]))
             await ctx.send(f'Your to-do list:\n{tasks}')
+
+    @commands.command()
+    async def ClearTodoList(self, ctx):
+        user_id = str(ctx.author.id)
+        todos[user_id] = []
+        await ctx.send("Todo list cleared!")
+
+    @commands.command()
+    async def RemoveTodoList(self, ctx, item):
+        user_id = str(ctx.author.id)
+        try:
+            todos[user_id].remove(item)
+            await ctx.send(f"{item} cleared!")
+        except:
+            await ctx.send(f"{item} is not in todo list.")
+        
     '''
     TODO
     Add the necessary bot commands here
